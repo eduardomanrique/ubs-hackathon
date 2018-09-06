688f5e14f88461ae7c95d9d550cb6ee6da7c3d28
Tiago Portela <tiagoporteladesouza@gmail.com>
2018-09-06 19:36:05 +0200

    Creating json to excel converter

---
+++ Json2Excel/src/main/java/com/ubs/hackaton/converter/dataobject/Data.java
@@ -0,0 +1,16 @@
+package com.ubs.hackaton.converter.dataobject;
+
+import java.util.List;
+
+public class Data {
+
+    private List<Opportunity> opportunities;
+
+    public List<Opportunity> getOpportunities() {
+        return opportunities;
+    }
+
+    public void setOpportunities(List<Opportunity> opportunities) {
+        this.opportunities = opportunities;
+    }
+}
