a4c505b76e26a3fc8322d67093011d28ad6170b5
eduardo <eduardo.manrique@gmail.com>
2018-09-06 15:51:01 +0200

    Rule engine

--- test/test/ClientApp/src/app/components/rule-group/rule-group.component.ts
+++ test/test/ClientApp/src/app/components/rule-group/rule-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import RuleEngine from '../../util/rule-engine';
 
 @Component({
   selector: 'app-rule-group',
@@ -9,22 +10,37 @@ export class RuleGroupComponent implements OnInit {
 
   constructor() { }
 
-  rules = [];
+  rules = [{
+    name: 'condition 1',
+    condition: '$entity.xyz == "ABC" and $entity.point == 3',
+    action: '$entity.evaluation = 10'
+},{
+    name: 'condition 2',
+    condition: '$entity.point == 2',
+    action: `
+$entity.evaluation = $entity.point * 2
+$entity.xyz = 'XYZ'
+    `
+},{
+    name: 'condition 3',
+    condition: '$entity.name == "Test3"',
+    action: '$entity.evaluation = $entity.evaluation + 2'
+}];
 
   pos = 0;
 
-  setPos(index){
+  setPos(index) {
     this.pos = index;
     console.log(index)
   }
 
-  textChangedHandler(event:any, rule:any) {
+  textChangedHandler(event: any, rule: any) {
     const val = event.target.value;
     const name = event.target.getAttribute('name');
     rule[name] = val;
   }
 
-  addRule(){
+  addRule() {
     this.rules.push({
       name: 'Unnamed',
       condition: '',
@@ -32,8 +48,17 @@ export class RuleGroupComponent implements OnInit {
     });
   }
 
-  removeRule(rule){
-    this.rules.splice(this.rules.indexOf(rule), 1);
+  removeRule(rule) {
+    //this.rules.splice(this.rules.indexOf(rule), 1);
+    const r = new RuleEngine(this.rules);
+    const e = {
+      name: 'Test3',
+      point: 2,
+      xyz: 'X',
+      evaluation: 0
+    };
+    r.exec(e)
+    console.log(JSON.stringify(e))
   }
 
   ngOnInit() {
