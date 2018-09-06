6de80dc6b85810adcf8f75134bd947e4e2be5721
Tigran Hakobyan <tigran.hakobyan@outlook.com>
2018-09-06 14:49:49 +0200

    sdd step

--- test/test/ClientApp/src/app/components/add-flow-page/add-flow-page.component.ts
+++ test/test/ClientApp/src/app/components/add-flow-page/add-flow-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { LocalStorageService } from "../../services/local-storage.service";
+import { FlowModel } from "../../models/flow.model";
+import { FlowStepModel } from "../../models/flowstep.model";
+import { FlowStepTypes } from "../../models/flowsteptypes.enum";
 
 @Component({
   selector: 'app-add-flow-page',
@@ -10,13 +13,42 @@ export class AddFlowPageComponent implements OnInit {
 
   @Output() cancelButtonClick: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  public model: FlowModel;
+
   constructor(private storageService: LocalStorageService) { }
 
   ngOnInit() {
+    this.model = new FlowModel("");
+
+    this.model.steps = [
+      new FlowStepModel("BPX", "http://www.mocky.io/v2/5b90ff993100008200939774"),
+      new FlowStepModel("BPX", "http://www.mocky.io/v2/5b90ff993100008200939774"),
+      new FlowStepModel("BPX", "http://www.mocky.io/v2/5b90ff993100008200939774")
+    ];
+  }
+
+  titleChangedHandler(val: string) {
+    this.model.title = val;
   }
 
   cancelBtnHandler() {
     this.cancelButtonClick.emit();
+  }
+
+  stepUrlChangedHandler(val, step) {
+    step.content = val;
+  }
+
+  removeStepHandler(index: number) {
+    this.model.steps.splice(index, 1);
+  }
+
+  addEmptyStepHandler(stepType: string) {
+    if (stepType === 'url') {
+      const newAPI = new FlowStepModel("BPX", "");
+      newAPI.stepType = FlowStepTypes.API;
+      this.model.steps.push(newAPI);
+    }
   }
 
   saveBtnHandler() {
