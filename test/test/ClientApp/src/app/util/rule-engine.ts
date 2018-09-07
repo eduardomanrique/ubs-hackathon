c08d64569aeee1d7de40afbb806d98ad23daa4b8
eduardo <eduardo.manrique@gmail.com>
2018-09-07 08:53:01 +0200

    Rule

--- test/test/ClientApp/src/app/util/rule-engine.ts
+++ test/test/ClientApp/src/app/util/rule-engine.ts
@@ -3,25 +3,34 @@ import coffeScript from 'coffeescript';
 export default class RuleEngine {
 
   rules: any[];
-    script: string;
+    script: string = null;
 
     constructor(rules: any[]){
         this.rules = rules;
-        let scripts = ['$entity.appliedRules = [];\n'];
-        scripts = scripts.concat(this.rules.map((rule, index) => {
-            const script = `if ${rule['content']}\n  $entity.appliedRules.push(${index})\n`;
-            return coffeScript.compile(script);
-        }));
-        scripts = scripts.concat(this.rules.map((rule, index) => {
-            const script = `if $entity.appliedRules.indexOf(${index}) >= 0\n${rule['action'].split('\n').map(line => `  ${line.trim()}`).join('\n')}\n`;
-            return coffeScript.compile(script);
-        }));
-        this.script = scripts.join('\n');
-        console.log(this.script)
     }
 
     exec($entity){
+        if(!this.script){
+            this.buildScript($entity);
+        }
         eval(this.script);
+    }
+
+    buildScript(e){
+        let scripts = ['$entity.appliedRules = [];\n'];
+        scripts = scripts.concat(this.rules.map((rule, index) => {
+            let script = Object.keys(e).map(k => `${k}=$entity.${k}`);
+            script.push(`if ${rule['content']}\n  $entity.appliedRules.push(${index})`);
+            return coffeScript.compile(script.join('\n'));
+        }));
+        scripts = scripts.concat(this.rules.map((rule, index) => {
+            let script = Object.keys(e).map(k => `${k}=$entity.${k}`);
+            script.push(`if $entity.appliedRules.indexOf(${index}) >= 0\n${rule['action'].split('\n').map(line => `  ${line.trim()}`).join('\n')}`);
+            script = script.concat(Object.keys(e).map(k => `$entity.${k}=${k}`));
+            return coffeScript.compile(script.join('\n'));
+        }));
+        this.script = scripts.join('\n');
+        console.log(this.script)
     }
 }
 
